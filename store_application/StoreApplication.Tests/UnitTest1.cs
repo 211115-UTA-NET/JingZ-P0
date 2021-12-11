468b73702e0079467627d76155fcf4c4666210ca
Jingjing Zhuang <jzhuang3219@gmail.com>
2021-12-11 18:32:46 -0500

    Initialized project framework.

---
+++ store_application/StoreApplication.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using StoreApplication;
+
+namespace StoreApplication.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ store_application/StoreApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StoreApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool continueShopping = true;
+            while (continueShopping)
+            {
+                Console.WriteLine("Which store do you want to shop in today?");
+            }
+        }
+    }
+}
