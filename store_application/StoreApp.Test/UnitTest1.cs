87b90c3ada59e939722d252907489b31e2e16110
Jingjing Zhuang <jzhuang3219@gmail.com>
2021-12-17 17:38:29 -0500

    initialize the project file

---
+++ store_application/StoreApp.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using StoreApplication;
+
+namespace StoreApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ store_application/Store_Application/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace StoreApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Staples");
+            bool continueShopping = false;
+            while (continueShopping)
+            {
+                Console.WriteLine("Which store do you want to shop in today?");
+            }
+        }
+
+    }
+}
